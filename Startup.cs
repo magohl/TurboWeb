c936a05d26b8d46acb548953d73f61cb4e8e081d
Magnus Ohlsson <magnus.ohlsson@gmail.com>
2019-04-04 08:50:55 +0200

    Added comment

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace TurboWeb
 {
+    //TEST
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
